75ebdba024c929be0485af538b58ba560dcd37e7
Steve Bedell <bedell.369@osu.edu>
2016-04-07 01:48:59 -0400

    updating anagram checker

--- CodeKatas/anagramChecker.rb
+++ CodeKatas/anagramChecker.rb
@@ -1,3 +1,5 @@
+require "benchmark"
+
 def checkAnagram(string1, string2)
     # Strip spaces and change to lowercase
     str1 = string1.delete(' ').downcase
@@ -24,6 +26,49 @@ def checkAnagram(string1, string2)
     return true
 end
 
+def checkAnagram2(string1, string2)
+    str1 = string1.delete(' ').downcase
+    str2 = string2.delete(' ').downcase
+
+    # For "perfect" anagrams
+    if str1.length != str2.length
+        return false
+    end
+
+    charMap1 = Hash.new
+    charMap2 = Hash.new
+
+    for x in str1.chars
+        if !charMap1.has_key?(x)
+            charMap1[x] = 1
+        else
+            charMap1[x] += 1
+        end
+    end
+    #puts charMap1
+
+    for y in str2.chars
+        if !charMap2.has_key?(y)
+            charMap2[y] = 1
+        else
+            charMap2[y] += 1
+        end
+    end
+
+    # Loop thru the sorted character arrays
+    # and if any chars don't match up, return false
+    i = 0
+    while i < charMap1.size
+        if (charMap1[i] != charMap2[i])
+            return false
+        end
+        i += 1
+    end
+
+    # Return true if it makes it thru all the checks
+    return true
+end
+
 # Testing
 puts "\nTesting notAnagram and test => " + checkAnagram("notAnagram", "test").to_s
 puts "test and seTT => \t\t" + checkAnagram("test", "seTT").to_s
@@ -33,3 +78,15 @@ puts "Anagram and a nag ram => " + checkAnagram("anagram", "a nag ram").to_s
 puts "Time and tide wait for no man. and Notified madman into water. => \t" +
     checkAnagram("Time and tide wait for no man.", "Notified madman into water.").to_s
 puts "Tom Marvolo Riddle and I am Lord Voldemort => \t" + checkAnagram("Tom Marvolo Riddle", "I am Lord Voldemort").to_s
+
+puts checkAnagram2("Tom Marvolo Riddle", "I am Lord Voldemort")
+puts Benchmark.measure { checkAnagram("Tom Marvolo Riddle", "I am Lord Voldemort") }
+puts Benchmark.measure { checkAnagram2("Tom Marvolo Riddle", "I am Lord Voldemort") }
+
+time1 = Time.now
+puts checkAnagram('Anagram', 'A nag ram')
+puts "Time elapsed = " + (Time.now - time1).to_s
+
+time2 = Time.now
+puts checkAnagram2('Anagram', 'A nag ram')
+puts "Time elapsed = " + (Time.now - time2).to_s
