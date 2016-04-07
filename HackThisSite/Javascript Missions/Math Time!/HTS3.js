42169ccc8877f312ca71a7cd9be8529904f37b9a
Steve Bedell <bedell.369@osu.edu>
2016-04-07 01:59:50 -0400

    tiny edit on old js page

--- HackThisSite/Javascript Missions/Math Time!/HTS3.js
+++ HackThisSite/Javascript Missions/Math Time!/HTS3.js
@@ -1,19 +1,19 @@
-function check( x ) {
-	var foo = 5 + 6 * 7;
-	document.getElementById("test").innerHTML = foo;
-	
-    var bar = foo % 8;
-	document.getElementById("test2").innerHTML = bar;
-	
-    var moo = bar * 2;
-	document.getElementById("test3").innerHTML = moo;
-	
-    var rar = moo / 3;
-	document.getElementById("test4").innerHTML = rar;
+function check(x) {
+	let foo = 5 + 6 * 7;
+	document.getElementById("test").innerText = foo;
+
+    let bar = foo % 8;
+	document.getElementById("test2").innerText = bar;
+
+    let moo = bar * 2;
+	document.getElementById("test3").innerText = moo;
+
+    let rar = moo / 3;
+	document.getElementById("test4").innerText = rar;
 
     if (x.length == moo) {
 		alert("win!");
-		window.location += "?lvl_password=" + x;
+		alert("?lvl_password=" + x);
 	} else {
 		alert("fail D:");
 	}
