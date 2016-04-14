102c66dacc6a7f0e6bfc08b1b9b4394fbf6f6efd
Steve Bedell <bedell.369@osu.edu>
2016-04-14 15:26:03 -0400

    updating temp kata

--- CodeKatas/tempKata.py
+++ CodeKatas/tempKata.py
@@ -7,28 +7,27 @@ with open("codeKataTemps.txt", "r") as f:
     #Input Line 1: N, the number of temperatures to analyze, 0 ≤ N < 1,000,000
     n = int(f.readline())
     assert n >= 0 and n < 1000000 # 0 ≤ N < 1,000,000
-        
+
     #Input Line 2: The N space delimited temperatures
     # expressed as integers ranging from -273 to 5526
     numbers = f.readline().strip().split(" ")
 
     # Display 0 (zero) if no temperatures are provided.
-    # Otherwise, display the temperature closest to 0.
     if n == 0:
         print(0)
-    else:
-        #assert len(numbers) == n, "number of elements must be equal to the N on line 1"
-        lowestTemp = -273
+    else:       # Otherwise, display the temperature closest to 0.
+        assert len(numbers) == n, "number of elements must be equal to the N on line 1"
+        lowestTemp = 5526
         for i in numbers:
             x = int(i)
-            #assert x > -273 and x < 5526, "integer temps must range from -273 to 5526"
+
             if x < -273 or x > 5526:
                 continue #skip the number
 
             if abs(x) < abs(lowestTemp):
                 lowestTemp = x
-            elif abs(x) == abs(lowestTemp):
-                if x > 0 and lowestTemp < 0:
-                    lowestTemp = x
+            elif (abs(x) == abs(lowestTemp)) and x > 0:
+                lowestTemp = x
 
+        print()
         print(lowestTemp)
