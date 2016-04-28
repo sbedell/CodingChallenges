d8df99b837866334830d2caf9bf34ad28bdcd8cb
Steve Bedell <bedell.369@osu.edu>
2016-04-28 13:58:17 -0400

    updating conways game of life

--- CodeKatas/conwaysLife/conwaysLife.js
+++ CodeKatas/conwaysLife/conwaysLife.js
@@ -1,25 +1,58 @@
 class ConwayBoard {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+    constructor(canvasElement) {
+        this.canvas = canvasElement;
+        this.context = canvasElement.getContext("2d");
+        this.context.fillStyle = "blue";
+        this.context.strokeStyle = "#e3e3e3";
+        
+        // 2d array filled with true or false values,
+        // true means the cell is/should be filled in:
         this.cells = [];
+    }
+    
+    drawGrid() {
+        for (let x = 0; x < this.canvas.height; x++) {
+            for (let y = 0; y < this.canvas.width; y++) {
+                this.context.beginPath();
+                this.context.rect(x * 10, y * 10, 10, 10);
+                this.context.stroke();
+            }
+        }
+    }
+    
+    clearCanvas() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    
+    drawSquare(xCoord, yCoord) {
+        this.context.fillRect(xCoord, yCoord, this.canvas.width / 2, this.canvas.height / 2);
+    }
+    
+    animateSquare() {
     }
 }
 
-function generateCanvas() {
+// Global Variable:
+let _theGameBoard;
+
+function initCanvas() {
+    // clear the canvas section first:
     document.getElementById("canvasSection").innerHTML = "";
 
     let newCanvas = document.createElement("canvas");
-    newCanvas.height = document.getElementById("height").value;
-    newCanvas.width = document.getElementById("width").value;
+    newCanvas.height = document.getElementById("height").value * 5;
+    newCanvas.width = document.getElementById("width").value * 5;
     newCanvas.id = "theCanvas";
 
     //Append it to the page:
     document.getElementById("canvasSection").appendChild(newCanvas);
+    
+    // use the constructor:
+    _theGameBoard = new ConwayBoard(newCanvas);
+    _theGameBoard.drawGrid();
+    _theGameBoard.drawSquare(10, 10);
+}
 
-    let context = document.getElementById("theCanvas").getContext("2d");
-
-    context.strokeStyle = "#e1e1e1";
-    context.fillStyle = "blue";
-    context.fillRect(10, 10, newCanvas.height / 2, newCanvas.width / 2);
+function clearCanvas() {
+    _theGameBoard.clearCanvas();
 }
